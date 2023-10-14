ae0b91eac7ff950d13d28d9b126885883b11bc3a
Nico <nico.hex6@gmail.com>
2023-10-14 10:56:12 +0200

    Fabric mod setup

---
+++ src/main/java/io/github/hexagonnico/undergroundjungle/UndergroundJungle.java
@@ -0,0 +1,11 @@
+package io.github.hexagonnico.undergroundjungle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UndergroundJungle implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
